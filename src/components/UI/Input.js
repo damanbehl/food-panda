ad1dadaab702558c0ad6c815711c6ba54da42e54
Daman Behl <damanbehl@gmail.com>
2021-06-25 13:40:44 -0400

    add to cart added

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,14 @@
+import React from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       {/* <input id={props.input.id} {...props.input} /> */}
       {/* If you look careully line 7 and 9 are the same id gets spread */}
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 export default Input;
