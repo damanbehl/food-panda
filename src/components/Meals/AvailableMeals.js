49b4e9628a15d9a08bab393646a5931b714a196c
Daman Behl <damanbehl@gmail.com>
2021-08-05 02:44:45 -0400

    api fetch meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,10 +1,32 @@
 import classes from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
-import DUMMY_MEALS from "./dummy-meals";
+// import DUMMY_MEALS from "./dummy-meals";
 import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "DUMMY LINK"
+      );
+      const responseData = await response.json();
+
+      const loadedData = [];
+      for (const key in responseData) {
+        loadedData.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedData);
+    };
+    fetchMeals();
+  }, []);
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
